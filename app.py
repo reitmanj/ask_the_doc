7ce18ed57f646fc7c079fcf937f77a64adddc2cb
Joe <jreitman4@icloud.com>
2023-10-07 06:33:48 -0500

    change #

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 __import__('pysqlite3')
 import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
+import sqlite3
+
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 def generate_response(uploaded_file, openai_api_key, query_text):
     
